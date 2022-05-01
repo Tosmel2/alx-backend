ff666cb40ebde148bc91a321bbfe103784ccbac6
Tosin Adewale <84603888+Tosmel2@users.noreply.github.com>
2022-05-02 00:32:02 +0100

    pagination task

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" 0. Simple helper function
+""" Write a function named index_range that takes
+two integer arguments page and page_size.
 """
 
-from typing import Tuple
 
+def index_range(page: int, page_size: int) -> tuple:
+    """ create a tuple containing page and the page_size """
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ Returns a tuple containing a start and end index.
-    """
-    return ((page - 1) * page_size, page * page_size)
-    
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
